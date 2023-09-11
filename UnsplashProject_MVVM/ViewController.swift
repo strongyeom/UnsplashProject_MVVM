08753a5ba99a57385d50a3c47d9b08e7fe5f35bc
yeomsungpil <present7256@gmail.com>
2023-09-11 12:34:29 +0900

    멀티 function 기능 - Ctrl + Shift + 클릭 적용해봄

--- UnsplashProject_MVVM/ViewController.swift
+++ UnsplashProject_MVVM/ViewController.swift
@@ -25,6 +25,35 @@ class ViewController: UIViewController {
 //        resultLabel.text = String(format: value, "고래밥","콩딱핑","다마고치")
 //
         resultLabel.text = "age_result".localized(number: 55)
+        
+        
+        // cmd + ctl + e
+        let badsdsdsdsdsdr = UISearchBar()
+        badsdsdsdsdsdr.text = "qweq"
+        badsdsdsdsdsdr.placeholder = "123132"
+        
+        
+        
+        
+        
+    }
+    
+    
+    // 멀티 클릭 기능
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#function) // Ctrl + Shift + 클릭
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function) // Ctrl + Shift + 클릭
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#function) // Ctrl + Shift + 클릭
     }
 
 
