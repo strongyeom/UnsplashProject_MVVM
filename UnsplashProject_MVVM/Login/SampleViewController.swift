c07b89de7d7fd28b355430521558dccde0e33f15
yeomsungpil <present7256@gmail.com>
2023-09-15 12:21:13 +0900

    Model을 클래스로 만들때 Hashable 적용하는 방법

--- UnsplashProject_MVVM/Login/SampleViewController.swift
+++ UnsplashProject_MVVM/Login/SampleViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 // Model에 Hashable 채택
-struct User: Hashable {
+// Model은 클래스보다 구조체를 사용!
+class User: Hashable {
+    
+    static func == (lhs: User, rhs: User) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    // id기준으로 고유성 판별해서 Hash함수 실행 
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
     
     let id = UUID().uuidString // Unique : name age가 같더라도 문제가 생기지 않게 처리를 해준다.
     let username: String
@@ -17,6 +28,12 @@ struct User: Hashable {
     
     var introduce: String {
         return  "\(username), \(age)살"
+    }
+    
+    init(username: String, age: Int, height: Int) {
+        self.username = username
+        self.age = age
+        self.height = height
     }
 }
 
