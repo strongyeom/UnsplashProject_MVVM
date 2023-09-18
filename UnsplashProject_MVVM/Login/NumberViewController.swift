3e24ac551377f53513bb9aacc809975d14a6a312
yeomsungpil <present7256@gmail.com>
2023-09-18 09:40:29 +0900

    MVVM 복습 시작

---
+++ UnsplashProject_MVVM/Login/NumberViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NumberViewController.swift
+//  UnsplashProject_MVVM
+//
+//  Created by 염성필 on 2023/09/18.
+//
+
+import UIKit
+
+class NumberViewController: UITabBarController {
+
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
